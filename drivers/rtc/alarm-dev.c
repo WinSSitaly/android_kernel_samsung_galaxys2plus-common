9fc964589361a51ba346f5794a6e02e93eee7db3
Xiaocheng Li <lix@codeaurora.org>
2014-04-04 12:20:59 +0530

    rtc:alarm: remove alarm_shutdown hook in alarm driver
    Putting alarm_shutdown into set_power_on_alarm is to set rtc alarm
    time immediately after an alarm being set. It can avoid shutting
    down unexpectedly before calling alarm_shutdown hook like removing
    battery.Then power_on_alarm will be set to rtc twice when an alarm
    is created and the device performs shutdown for a power off alarm.
    The set process during shutting down is duplicated to the set
    process when an alarm is created. So the alarm_shutdown hook become
    unnecessary.
    
    CRs-Fixed: 639119
    Change-Id: Ie46bcf6f568f04650d94ba40ad6d4521d31a2f96
    Signed-off-by: Xiaocheng Li <lix@codeaurora.org>

--- drivers/rtc/alarm-dev.c
+++ drivers/rtc/alarm-dev.c
@@ -111,11 +111,11 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 				wake_unlock(&alarm_wake_lock);
 		}
 		alarm_enabled &= ~alarm_type_mask;
+		spin_unlock_irqrestore(&alarm_slock, flags);
 		if (alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP)
 			if (!copy_from_user(&new_alarm_time,
 				(void __user *)arg, sizeof(new_alarm_time)))
 				set_power_on_alarm(new_alarm_time.tv_sec, 0);
-		spin_unlock_irqrestore(&alarm_slock, flags);
 		break;
 
 	case ANDROID_ALARM_SET_OLD:
@@ -149,12 +149,12 @@ from_old_alarm_set:
 		alarm_start_range(&alarms[alarm_type],
 			timespec_to_ktime(new_alarm_time),
 			timespec_to_ktime(new_alarm_time));
+                spin_unlock_irqrestore(&alarm_slock, flags);
                 if ((alarm_type == ANDROID_ALARM_RTC_POWEROFF_WAKEUP) &&
 				(ANDROID_ALARM_BASE_CMD(cmd) ==
 				 ANDROID_ALARM_SET(0)))
 			set_power_on_alarm(new_alarm_time.tv_sec, 1);
 #endif /* CONFIG_BCM_RTC_ALARM_BOOT */
-		spin_unlock_irqrestore(&alarm_slock, flags);
 #ifdef CONFIG_BCM_RTC_ALARM_BOOT
 		if (alarm_type == ANDROID_ALARM_RTC_POWERUP)
 			alarm_powerup_set_alarm(new_alarm_time);
